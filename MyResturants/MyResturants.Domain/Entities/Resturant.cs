264e3c3d6e22509ba59a41422a50c6476e9b8b0b
Omar Ahmed <o.ahmed9847@gmail.com>
2025-03-15 21:09:05 +0200

    Init

---
+++ MyResturants/MyResturants.Domain/Entities/Resturant.cs
@@ -0,0 +1,21 @@
+﻿namespace MyResturants.Domain.Entities;
+
+public class Resturant
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+
+    public bool HasDelivery { get; set; }
+
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+
+    public Address Address { get; set; } = new(); // Owned entity, no separate table
+    public List<Dish> Dishes { get; set; } = new();
+
+    //public User Owner { get; set; } = default!;
+    //public string OwnerId { get; set; } = default!;
+    //public string? LogoUrl { get; set; }
+}
---
+++ MyResturants/MyResturants.Infrastructure/Presistance/ResturantsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyResturants.Domain.Entities;
+
+namespace MyResturants.Infrastructure.Presistance;
+
+public class ResturantsDbContext : DbContext
+{
+    public DbSet<Resturant> Resturants { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+        .UseSqlServer("Server=.;Database=MyResturantsDb;Trusted_Connection=True;Trust Server Certificate=True;MultipleActiveResultSets=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Resturant>().OwnsOne(r => r.Address, a =>
+        {
+            //a.Property(a => a.Street).IsRequired();
+        });
+
+
+        modelBuilder.Entity<Resturant>()
+            .HasMany(r => r.Dishes).WithOne(d => d.Resturant)
+            .HasForeignKey(d => d.RestaurantId);
+
+        modelBuilder.Entity<Dish>()
+           .Property(d => d.Price)
+           .HasPrecision(18, 2);  
+
+    }
+}
